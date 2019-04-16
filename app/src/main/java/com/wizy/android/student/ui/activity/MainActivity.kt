5705b0bf9b608b9b8986e1c64f8dbeb9e863eb54
Muhammad Hamza <humxamoghal@gmail.com>
2019-04-16 13:28:44 +0500

    Little bit improved

--- app/src/main/java/com/wizy/android/student/ui/activity/MainActivity.kt
+++ app/src/main/java/com/wizy/android/student/ui/activity/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.view.View
 import com.wizy.android.student.R
 import com.wizy.android.student.base.BaseActivity
+import com.wizy.android.student.helper.Preferences
+import com.wizy.android.student.model.Student
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity(), View.OnClickListener {
-
+    private var student: Student? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +20,11 @@ class MainActivity : BaseActivity(), View.OnClickListener {
     private fun setUpView() {
         searchSkillMaster.setOnClickListener(this)
         customLearning.setOnClickListener(this)
+        student = Preferences.getInstance().student
+        student?.let {
+            tvName.text = it.firstName
+        }
+
     }
 
     override fun onClick(v: View?) {
